7fad88aa36e95f3fb956a3b6b621e1894f3665f6
Rishav Kumar <rishavkr43125@gmail.com>
2025-06-28 00:08:29 +0530

    deployment

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app,db
 from app.models import Task
 
@@ -7,4 +8,8 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+    
